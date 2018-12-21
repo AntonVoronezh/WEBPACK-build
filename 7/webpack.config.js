7200332286d40951a55521a80a99219a7253ded6
AntonVoronezh <pstut@mail.ru>
2018-12-21 08:45:55 +0300

    maps

---
+++ 7/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+
+    context: path.resolve(__dirname, 'src'),
+
+    entry: './a',
+
+    output: {
+
+        filename: 'bundle.js',
+
+        path: path.resolve(__dirname, 'dist')
+
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+
+        extensions: ['.js']
+
+    },
+
+    watch: false
+
+};
